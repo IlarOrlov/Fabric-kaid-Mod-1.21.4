8e42a5cf16cb3a7d6ab960b8f9a6f626b18a7466
Errant Wanderer <ilar.foremost@gmail.com>
2025-04-12 00:44:27 +0300

    Initial commit

---
+++ src/main/java/net/errantwanderer/kaid/KAIDDataGenerator.java
@@ -0,0 +1,11 @@
+package net.errantwanderer.kaid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KAIDDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
